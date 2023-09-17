a92f15003434a18079ec695a7c279e51ac4149bd
Ashani <Ashani9696>
2023-09-16 23:35:58 -0700

    commit

---
+++ View/DashboardView.swift
@@ -0,0 +1,31 @@
+//
+//  DashboardView.swift
+//  login
+//
+//  Created by malindu pabasara on 2023-09-14.
+//
+
+import SwiftUI
+import SwiftUICharts
+
+struct DashboardView: View {
+    var body: some View {
+        VStack {
+                    Text("Your Financial Status")
+                        .font(.title)
+                    
+                    BarChartView(data: ChartData(values: [
+                        ("Income", 500),
+                        ("Expenses", 300),
+                        ("Savings", 200)
+                    ]), title: "Monthly Summary", legend: "In Dollars")
+                    .padding()
+                }
+    }
+}
+
+struct DashboardView_Previews: PreviewProvider {
+    static var previews: some View {
+        DashboardView()
+    }
+}
---
+++ View/depe.swift
@@ -0,0 +1,20 @@
+//
+//  depe.swift
+//  login
+//
+//  Created by malindu pabasara on 2023-09-14.
+//
+
+import SwiftUI
+
+struct depe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct depe_Previews: PreviewProvider {
+    static var previews: some View {
+        depe()
+    }
+}
