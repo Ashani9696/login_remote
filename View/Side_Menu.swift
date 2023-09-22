04f0f1bc985feff7bbe46d838eb82e74ce45fc69
Ashani <Ashani9696>
2023-09-22 11:28:30 -0700

    side_Menu

--- View/Side_Menu.swift
+++ View/Side_Menu.swift
@@ -7,19 +7,84 @@
 
 import SwiftUI
 
+struct  MenuItem: Identifiable{
+
+    var id = UUID()
+    let text: String
+    let handler: () ->Void = {
+        print("Tapped itrms")
+    }
+}
+
 struct  MenuContent: View {
+    let items: [MenuItem] = [
+ MenuItem(text: "Home"),
+        MenuItem(text: "Hom"),
+        MenuItem(text: "Ho"),
+        MenuItem(text: "settings"),
+        MenuItem(text: "profile"),
+        MenuItem(text: "reports"),
+        ]
+    
     var body: some View{
         ZStack{
+            Color(UIColor(red: 33/255.0, green: 3/255.0, blue: 3/255.0, alpha: 1))
             
-            
+            VStack(alignment: .leading, spacing: 0){
+                ForEach(items) { item in
+                    HStack{
+                        Text(item.text)
+                            .foregroundColor(Color.black)
+                            .bold()
+                            .font(.system(size: 22))
+                            .multilineTextAlignment(/*@START_MENU_TOKEN@*/.leading/*@END_MENU_TOKEN@*/)
+                        
+                        Spacer()
+                    }
+                    .onTapGesture {
+                        
+                    }
+                    .padding()
+                    
+                    
+                    Divider()
+                    
+                }
+                Spacer()
+            }
+            .padding(.top, 25)
         }
     }
 }
 
 struct  SideMenu: View {
+    let width: CGFloat
+    let menuOpened: Bool
+    let toggleMenu: ()->Void
+    
     var body: some View{
         ZStack{
+            //dimed the background view
+            GeometryReader { _ in
+                EmptyView()
+                
+            }
+            .background(Color.gray.opacity(0.25))
+            .opacity(self.menuOpened ? 1: 0)
+            .animation(Animation.easeIn.delay(0.25))
+            .onTapGesture {
+                self.toggleMenu()
+            }
             
+            //Menu content
+            HStack{
+                MenuContent()
+                    .frame(width: width)
+                    .offset(x: menuOpened ? 0: -width)
+                    .animation(.default)
+                
+                Spacer()
+            }
             
         }
     }
@@ -29,19 +94,26 @@ struct Side_Menu: View {
     @State var menuOpened = false
     var body: some View {
         ZStack{
-            Button(action: {
-                //oprn menu
-                self.menuOpened.toggle()
+            if !menuOpened {
+                Button(action: {
+                    //oprn menu
+                    self.menuOpened.toggle()
+                    
+                }, label: {
+                    Text("Open Menu")
+                        .bold()
+                        .foregroundColor(Color.black)
+                        .frame(width: 200, height: 50, alignment: .center)
+                        .background(Color(.systemBlue))
+                })
                 
-            }, label: {
-                Text("Open Menu")
-                    .bold()
-                    .foregroundColor(Color.white)
-                    .frame(width: 200, height: 50, alignment: .center)
-                    .background(Color(.systemBlue))
-            })
+            }
+            SideMenu(width: UIScreen.main.bounds.width/1.8, menuOpened: menuOpened, toggleMenu: toggleMenu)
         }
         .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+    func toggleMenu(){
+        menuOpened.toggle()
     }
 }
 
